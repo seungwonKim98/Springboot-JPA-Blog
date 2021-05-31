8fc67074d85fb4c364401ab253dedc76a814ffc7
seungwonKim98 <godswan1998@gmail.com>
2021-05-31 23:54:24 +0900

    오류 캐치 완료

--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -4,6 +4,8 @@ import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -21,9 +23,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 @Entity //User 클래스가 mysql에 테이블이 생성이 된다 
-
+//@DynamicInsert // 
 public class User {
-
+  
 	@Id //Primary key
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 연결된 DB넘버링 전략을 따라간다
 	private int id; // 오라클 = 시퀀스, mysql = auto_increment
@@ -37,8 +39,9 @@ public class User {
 	@Column(nullable = false, length = 50)
 	private String email;
 	
-	@ColumnDefault("'user'")
-	private String role; // Enum을 쓰는게 좋다. // 회원 가입을 했을때 이사람의 role 은 admin, user, manager 
+	//@ColumnDefault("user")
+	@Enumerated(EnumType.STRING)
+	private RoleType role; // Enum을 쓰는게 좋다. // 회원 가입을 했을때 이사람의 role 은 ADMIN,USER
 	
 	@CreationTimestamp // 시간이 자동으로 입력
 	private Timestamp createDate;
